9bb05adf17165bff109d1ca383710478cb2fd5f4
ThankNgan <ntthanhngan.2001@gmail.com>
2026-05-13 01:12:42 +0700

    update loginTestSuccess & UpdateAccountTest

--- src/test/java/com/Bai16_LoginTest.java
+++ src/test/java/com/Bai16_LoginTest.java
@@ -3,6 +3,7 @@ package com;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import com.utils.BasicTest;
@@ -10,7 +11,7 @@ import com.utils.Utils;
 
 public class Bai16_LoginTest extends BasicTest {
 
-    @Test()
+    @Test(priority = 1)
     public void loginTestSuccess() throws Exception {
         // Launch website and navigate to https://bantheme.xyz/hathanhauto/tai-khoan/
         String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
@@ -22,10 +23,12 @@ public class Bai16_LoginTest extends BasicTest {
         WebElement loginPasswordFieldLocator = driver.findElement(By.id("password"));
         WebElement loginButtonLocator = driver.findElement(By.cssSelector("button[name='login']"));
 
+        String emailLogin = "ntthanhngan.2001@gmail.com";
+        String passwordLogin = "Thanhngan@123456";
         // Enter email address into email field
-        loginEmailFieldLocator.sendKeys("ntthanhngan.2001@gmail.com");
+        loginEmailFieldLocator.sendKeys(emailLogin);
         // Enter password into password field
-        loginPasswordFieldLocator.sendKeys("Thanhngan@123456");
+        loginPasswordFieldLocator.sendKeys(passwordLogin);
         // Click login button
         loginButtonLocator.click();
         Utils.hardWait(3000);
@@ -49,8 +52,9 @@ public class Bai16_LoginTest extends BasicTest {
         WebElement loginPasswordFieldLocator = driver.findElement(By.id("password"));
         WebElement loginButtonLocator = driver.findElement(By.cssSelector("button[name='login']"));
 
+        String emailLogin = "ntthanhngan.2001@gmail.com";
         // Enter email address into email field
-        loginEmailFieldLocator.sendKeys("ntthanhngan.2001@gmail.com");
+        loginEmailFieldLocator.sendKeys(emailLogin);
         // Do not enter password into password field
         // Click login button
         loginButtonLocator.click();
--- src/test/java/com/Bonus_UpdateAccountTest.java
+++ src/test/java/com/Bonus_UpdateAccountTest.java
@@ -3,6 +3,7 @@ package com;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import com.utils.BasicTest;
@@ -22,8 +23,10 @@ public class Bonus_UpdateAccountTest extends BasicTest {
         WebElement loginPasswordFieldLocator = driver.findElement(By.id("password"));
         WebElement loginButtonLocator = driver.findElement(By.cssSelector("button[name='login']"));
 
-        loginEmailFieldLocator.sendKeys("ntthanhngan.2001@gmail.com");
-        loginPasswordFieldLocator.sendKeys("Thanhngan@123456");
+        String emailLogin = "ntthanhngan.2001@gmail.com";
+        String passwordLogin = "Thanhngan@123456";
+        loginEmailFieldLocator.sendKeys(emailLogin);
+        loginPasswordFieldLocator.sendKeys(passwordLogin);
         loginButtonLocator.click();
 
         WebElement contentTextLocator = driver.findElement(By.className("woocommerce-MyAccount-content"));
@@ -34,11 +37,17 @@ public class Bonus_UpdateAccountTest extends BasicTest {
         driver.findElement(By.cssSelector("a[href*='edit-account']")).click();
         // Navigate to the DisplayName field
         WebElement displayNameFieldLocator = driver.findElement(By.name("account_display_name"));
-        displayNameFieldLocator.sendKeys("Thanh Ngân");
+        String originalDisplayName = displayNameFieldLocator.getAttribute("value");
+        String newDisplayName = originalDisplayName + "_updated";
+        displayNameFieldLocator.clear();
+        displayNameFieldLocator.sendKeys(newDisplayName);
         // Navigate to the Email field
         WebElement emailAccountFieldLocator = driver.findElement(By.name("account_email"));
+        String originalEmail = "ntthanhngan.2001@gmail.com";
+        String newEmail = originalEmail.substring(0, originalEmail.indexOf("@")) + "_updated"
+                + originalEmail.substring(originalEmail.indexOf("@"));
         emailAccountFieldLocator.clear();
-        emailAccountFieldLocator.sendKeys("thanhngan@gmail.com");
+        emailAccountFieldLocator.sendKeys(newEmail);
         // Save the updates
         driver.findElement(By.name("save_account_details")).click();
         Utils.hardWait();
@@ -46,6 +55,21 @@ public class Bonus_UpdateAccountTest extends BasicTest {
         WebElement alertMessageLocator = driver.findElement(By.className("woocommerce-message"));
         Assert.assertTrue(alertMessageLocator.getText().contains("Thông tin tài khoản đã được cập nhật"));
 
+        revertUpdates(originalDisplayName, originalEmail);
+
+    }
+
+    public void revertUpdates(String originalDisplayName, String originalEmail) {
+        // Revert updates
+        driver.findElement(By.cssSelector("a[href*='edit-account']")).click();
+        WebElement displayNameFieldLocator = driver.findElement(By.name("account_display_name"));
+        displayNameFieldLocator.clear();
+        displayNameFieldLocator.sendKeys(originalDisplayName);
+        WebElement emailAccountFieldLocator = driver.findElement(By.name("account_email"));
+        emailAccountFieldLocator.clear();
+        emailAccountFieldLocator.sendKeys(originalEmail);
+        driver.findElement(By.name("save_account_details")).click();
+        Utils.hardWait();
     }
 
 }
