45892815ebe9bdc56d14139f045c3541de09e3e1
ThankNgan <ntthanhngan.2001@gmail.com>
2026-05-16 19:23:54 +0700

    update BasicTest

--- src/main/java/com/utils/BasicTest.java
+++ src/main/java/com/utils/BasicTest.java
@@ -1,18 +1,20 @@
 package com.utils;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.concurrent.TimeUnit;
 
-
 public abstract class BasicTest {
-    
+
     public static final Logger logger = LogManager.getLogger();
     protected static WebDriver driver;
     // private String driverPath;
@@ -28,12 +30,46 @@ public abstract class BasicTest {
         driver = new ChromeDriver();
         // Maximize the browser
         driver.manage().window().maximize();
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterMethod
-    public void postCondition(){
+    public void postCondition() {
         // Quit the Browser
         driver.quit();
+    }
+
+    public void login(String username, String password) {// Launch website and navigate to
+                                                         // https://bantheme.xyz/hathanhauto/tai-khoan/
+        String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        // Declare locator
+        WebElement loginEmailFieldLocator = driver.findElement(By.id("username"));
+        WebElement loginPasswordFieldLocator = driver.findElement(By.id("password"));
+        WebElement loginButtonLocator = driver.findElement(By.cssSelector("button[name='login']"));
+
+        // Enter email address into email field
+        loginEmailFieldLocator.sendKeys(username);
+        // Enter password into password field
+        loginPasswordFieldLocator.sendKeys(password);
+        // Click login button
+        loginButtonLocator.click();
+        Utils.hardWait(3000);
+    }
+
+    public WebElement getElementLocator(By locator) {
+        return driver.findElement(locator);
+    }
+
+    public void navigateTo(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    public void setData(By locator, String value) {
+        WebElement element = driver.findElement(locator);
+        element.clear();
+        element.sendKeys(value);
     }
 }
