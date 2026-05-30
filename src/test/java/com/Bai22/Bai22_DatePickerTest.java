7accdd4a726a324a8f8bf9b918cd1f395a27b05f
ThankNqan <ntthanhngan.2001@gmail.com>
2026-05-30 15:40:42 +0700

    update project

--- src/test/java/com/Bai22/Bai22_DatePickerTest.java
+++ src/test/java/com/Bai22/Bai22_DatePickerTest.java
@@ -1,27 +1,27 @@
-package com.Bai22;
+// package com.Bai22;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+// import org.testng.Assert;
+// import org.testng.annotations.Test;
 
-import com.pages.HomePageIvivu;
-import com.utils.BasicTest;
-import com.utils.Utils;
+// import com.pages.HomePageIvivu;
+// import com.utils.BasicTest;
+// import com.utils.Utils;
 
-public class Bai22_DatePickerTest extends BasicTest {
+// public class Bai22_DatePickerTest extends BasicTest {
 
-    HomePageIvivu homePage;
-    String url = "https://www.ivivu.com/";
-    String location = "Phú Quốc";
+// HomePageIvivu homePage;
+// String url = "https://www.ivivu.com/";
+// String location = "Phú Quốc";
 
-    @Test()
-    public void datePickerTest() {
-        driver.get(url);
-        homePage = new HomePageIvivu(driver);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+// @Test()
+// public void datePickerTest() {
+// driver.get(url);
+// homePage = new HomePageIvivu(driver);
+// Assert.assertEquals(driver.getCurrentUrl(), url);
 
-        homePage.bookingTravel(location);
-        Utils.hardWait(2000);
-        homePage.selectDate("22", "Tháng 6", "2026");
+// homePage.bookingTravel(location);
+// Utils.hardWait(2000);
+// homePage.selectDate("22", "Tháng 6", "2026");
 
-    }
-}
+// }
+// }
